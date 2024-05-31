d683db2ea74f4d340bd9adbbe5227da3faca87e3
ZakiSafi <zakiullahsafi00@gmail.com>
2024-05-31 21:47:50 +0430

    update

--- starter/script.js
+++ starter/script.js
@@ -5,15 +5,18 @@
 // document.querySelector('.score').textContent = '9'
 // document.querySelector('.highscore').textContent = '8'
 
-const random = Math.trunc(Math.random()*20) +1;
-document.querySelector('.number').textContent = random;
+let random = Math.trunc(Math.random()*20) +1;
 let score = 20;
+let highscore = 0;
 document.querySelector('.check').addEventListener('click',function(){
     const guess = document.querySelector('.guess').value;
     console.log(random);
+    
+    // when there is no guess 
     if(!guess){
         document.querySelector('.message').textContent = 'NO number to guess';
     }
+    // when the guess is less than the random value
     else if(guess < random){
         if(score  >1){
             document.querySelector('.message').textContent = 'Too short';
@@ -26,6 +29,7 @@ document.querySelector('.check').addEventListener('click',function(){
         }
         
     }
+    // when the guess is greater than the random number
     else if(guess> random){
         if(score >1){
             document.querySelector('.message').textContent = 'to great';
@@ -37,7 +41,27 @@ document.querySelector('.check').addEventListener('click',function(){
             document.querySelector('.score').textContent = score -1;
         }
     }
+    // when the guess is equal to the random number
     else{
         document.querySelector('.message').textContent = 'Correct Number';
+        document.querySelector('.number').textContent = random;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
     }
+})
+document.querySelector('.again').addEventListener('click',function(){
+    score = 20;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    random = Math.trunc(Math.random()*20) +1;
+
 })
